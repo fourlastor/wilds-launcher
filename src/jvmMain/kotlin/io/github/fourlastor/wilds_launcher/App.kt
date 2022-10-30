7ef6c820390f0d71cd3b3e3e38da3863105278ae
julcreutz <julcreutz@gmail.com>
2022-10-30 16:08:01 +0100

    Remove "MaterialTheme" wrapping the App content since it's not needed

--- src/jvmMain/kotlin/io/github/fourlastor/wilds_launcher/App.kt
+++ src/jvmMain/kotlin/io/github/fourlastor/wilds_launcher/App.kt
@@ -1,7 +1,6 @@
 package io.github.fourlastor.wilds_launcher
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import io.github.fourlastor.wilds_launcher.settings.SettingsState
 import io.github.fourlastor.wilds_launcher.settings.SettingsViewModel
@@ -14,48 +13,47 @@ import org.koin.core.component.inject
 @Preview
 fun App(appComponent: AppComponent, getPokeWildsLocation: () -> Pair<String, String>?) {
     val viewModel = appComponent.settingsViewModel
-    MaterialTheme {
-        ViewModelContainer(viewModel) { settingsState ->
-            when (settingsState) {
-                is SettingsState.Loaded -> Launcher(
-                    viewModel,
-                    settingsState = settingsState,
-                    onDevModeChanged = {
-                        viewModel.devMode(it)
-                    },
-                    onLogsEnabledChanged = {
-                        viewModel.logsEnabled(it)
-                    },
-                    onAngleGles20Changed = {
-                        viewModel.angleGles20(it)
-                    },
-                    runPokeWilds = { state ->
-                        if (state.logsEnabled) {
-                            runPokeWilds(viewModel, state) { state.appendLog(it) }
-                        }
-                        else {
-                            runPokeWilds(viewModel, state)
-                        }
-                    },
-                    clearData = {
-                        viewModel.clearData()
+
+    ViewModelContainer(viewModel) { settingsState ->
+        when (settingsState) {
+            is SettingsState.Loaded -> Launcher(
+                viewModel,
+                settingsState = settingsState,
+                onDevModeChanged = {
+                    viewModel.devMode(it)
+                },
+                onLogsEnabledChanged = {
+                    viewModel.logsEnabled(it)
+                },
+                onAngleGles20Changed = {
+                    viewModel.angleGles20(it)
+                },
+                runPokeWilds = { state ->
+                    if (state.logsEnabled) {
+                        runPokeWilds(viewModel, state) { state.appendLog(it) }
                     }
-                )
+                    else {
+                        runPokeWilds(viewModel, state)
+                    }
+                },
+                clearData = {
+                    viewModel.clearData()
+                }
+            )
 
-                is SettingsState.Downloading -> Downloader(viewModel)
+            is SettingsState.Downloading -> Downloader(viewModel)
 
-                is SettingsState.CheckingForUpdates -> UpdateChecker(viewModel)
-                is SettingsState.NoUpdatesFound -> OkDialog(arrayOf("No available update found.")) { viewModel.manager.update { SettingsState.Missing } }
+            is SettingsState.CheckingForUpdates -> UpdateChecker(viewModel)
+            is SettingsState.NoUpdatesFound -> OkDialog(arrayOf("No available update found.")) { viewModel.manager.update { SettingsState.Missing } }
 
-                is SettingsState.UpdateFound -> YesNoDialog(
-                    arrayOf("There is an update available (${getLatestReleaseVersion()}).", "Do you want to download it?"),
-                    { viewModel.manager.update { SettingsState.Downloading } },
-                    { viewModel.manager.update { SettingsState.Missing } }
-                )
+            is SettingsState.UpdateFound -> YesNoDialog(
+                arrayOf("There is an update available (${getLatestReleaseVersion()}).", "Do you want to download it?"),
+                { viewModel.manager.update { SettingsState.Downloading } },
+                { viewModel.manager.update { SettingsState.Missing } }
+            )
 
-                is SettingsState.Missing -> JarPicker(getPokeWildsLocation, viewModel)
-                is SettingsState.Loading -> {}
-            }
+            is SettingsState.Missing -> JarPicker(getPokeWildsLocation, viewModel)
+            is SettingsState.Loading -> {}
         }
     }
 }
