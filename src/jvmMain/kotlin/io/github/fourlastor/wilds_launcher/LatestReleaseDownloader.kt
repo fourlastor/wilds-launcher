79891cd8d0b3862d7e5c0baf98062931664d5362
julcreutz <julcreutz@gmail.com>
2022-10-30 10:01:39 +0100

    Get the latest version using a GET request on the GitHub API

--- src/jvmMain/kotlin/io/github/fourlastor/wilds_launcher/LatestReleaseDownloader.kt
+++ src/jvmMain/kotlin/io/github/fourlastor/wilds_launcher/LatestReleaseDownloader.kt
@@ -1,16 +1,49 @@
 package io.github.fourlastor.wilds_launcher
 
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
 import java.io.BufferedInputStream
 import java.io.File
 import java.io.FileOutputStream
+import java.net.HttpURLConnection
 import java.net.URL
 
 const val READ_SIZE_IN_BYTES = 1024
 
+fun getLatestVersion() : String? {
+    val url = URL("https://api.github.com/repos/SheerSt/pokewilds/releases/latest")
+    val connection = url.openConnection() as HttpURLConnection
+
+    connection.requestMethod = "GET"
+    connection.setRequestProperty("accept", "application/vnd.github+json")
+
+    connection.connectTimeout = 5000
+    connection.readTimeout = 5000
+
+    if (connection.responseCode >= 300) {
+        return null;
+    }
+
+    val response = connection.inputStream.bufferedReader().use { it.readText() }
+    val jsonObject = Json.parseToJsonElement(response) as JsonObject
+
+    val jsonPrimitive = jsonObject["name"] as JsonPrimitive
+    val version = jsonPrimitive.content
+
+    return version
+}
+
 fun downloadLatestRelease() {
-    val version = "v0.8.6"
+    val latestVersion = getLatestVersion()
+
+    if (latestVersion == null) {
+        println("Could not get latest version.")
+        return;
+    }
+
     val filename = "pokemon-wilds-windows64.zip"
-    val url = URL("https://github.com/SheerSt/pokewilds/releases/download/$version/$filename")
+    val url = URL("https://github.com/SheerSt/pokewilds/releases/download/$latestVersion/$filename")
     val connection = url.openConnection()
     val totalBytesToDownload = connection.contentLength
 
