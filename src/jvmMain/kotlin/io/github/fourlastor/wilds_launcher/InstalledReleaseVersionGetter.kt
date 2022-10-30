be59e6737fdcd025e272d645c4c14f338f29af8c
julcreutz <julcreutz@gmail.com>
2022-10-30 13:07:24 +0100

    Implement InstalledReleaseVersionGetter

---
+++ src/jvmMain/kotlin/io/github/fourlastor/wilds_launcher/InstalledReleaseVersionGetter.kt
@@ -0,0 +1,13 @@
+package io.github.fourlastor.wilds_launcher
+
+import io.github.fourlastor.wilds_launcher.settings.SettingsState
+import java.io.File
+
+fun getInstalledReleaseVersion(state: SettingsState.Loaded): String {
+    val file = File(state.dir)
+    val parent = file.parentFile
+    val name = parent.name
+    val parts = name.split('-')
+
+    return parts[1]
+}
