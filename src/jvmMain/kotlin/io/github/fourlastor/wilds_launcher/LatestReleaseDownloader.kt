715ca1a7f52e8184f2c443fda2f2222b897a1a11
julcreutz <julcreutz@gmail.com>
2022-10-30 12:09:36 +0100

    Extract code to get the latest release's version into LatestReleaseVersionGetter

--- src/jvmMain/kotlin/io/github/fourlastor/wilds_launcher/LatestReleaseDownloader.kt
+++ src/jvmMain/kotlin/io/github/fourlastor/wilds_launcher/LatestReleaseDownloader.kt
@@ -1,59 +1,16 @@
 package io.github.fourlastor.wilds_launcher
 
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.JsonPrimitive
 import java.io.BufferedInputStream
 import java.io.File
 import java.io.FileOutputStream
-import java.net.HttpURLConnection
 import java.net.URL
-
-const val OWNER = "SheerSt"
-const val REPOSITORY = "pokewilds"
 
 const val READ_SIZE_IN_BYTES = 1024
 
-fun getLatestVersion() : String? {
-    val url = URL("https://api.github.com/repos/$OWNER/$REPOSITORY/releases/latest")
-    val connection = url.openConnection() as HttpURLConnection
-
-    connection.requestMethod = "GET"
-    connection.setRequestProperty("accept", "application/vnd.github+json")
-
-    connection.connectTimeout = 5000
-    connection.readTimeout = 5000
-
-    if (connection.responseCode >= 300) {
-        return null
-    }
-
-    val response = connection.inputStream.bufferedReader().use { it.readText() }
-    val jsonObject = Json.parseToJsonElement(response) as JsonObject
-
-    val jsonPrimitive = jsonObject["name"] as JsonPrimitive
-    val version = jsonPrimitive.content
-
-    return version
-}
-
-private fun getFilenameWithoutExtension() : String {
-    // TODO: What is the difference?
-
-    //val filenameWithoutExtension = "pokewilds-"
-    val filenameWithoutExtension = "pokemon-wilds-"
-
-    return when (getOperatingSystem()) {
-        OperatingSystem.Windows -> filenameWithoutExtension + "windows64"
-        OperatingSystem.Linux -> filenameWithoutExtension + "linux64"
-        else -> filenameWithoutExtension + "otherplatforms"
-    }
-}
-
 fun downloadLatestRelease() : File? {
-    val latestVersion = getLatestVersion()
+    val version = getLatestReleaseVersion()
 
-    if (latestVersion == null) {
+    if (version == null) {
         println("Could not get latest version.")
         return null
     }
@@ -61,7 +18,7 @@ fun downloadLatestRelease() : File? {
     val filenameWithoutExtension = getFilenameWithoutExtension()
     val filename = "$filenameWithoutExtension.zip"
 
-    val url = URL("https://github.com/$OWNER/$REPOSITORY/releases/download/$latestVersion/$filename")
+    val url = URL("https://github.com/SheerSt/pokewilds/releases/download/$version/$filename")
     val connection = url.openConnection()
     val totalBytesToDownload = connection.contentLength
 
@@ -95,4 +52,17 @@ fun downloadLatestRelease() : File? {
     println("Unzipped archive.")
 
     return destination
+}
+
+private fun getFilenameWithoutExtension() : String {
+    // TODO: What is the difference?
+
+    //val filenameWithoutExtension = "pokewilds-"
+    val filenameWithoutExtension = "pokemon-wilds-"
+
+    return when (getOperatingSystem()) {
+        OperatingSystem.Windows -> filenameWithoutExtension + "windows64"
+        OperatingSystem.Linux -> filenameWithoutExtension + "linux64"
+        else -> filenameWithoutExtension + "otherplatforms"
+    }
 }
