551bdc5bca611827c6e68b76eebd13f97fc4eba7
julcreutz <julcreutz@gmail.com>
2022-10-30 10:14:39 +0100

    Unzip the downloaded zip file and delete the zip afterwards

--- src/jvmMain/kotlin/io/github/fourlastor/wilds_launcher/LatestReleaseDownloader.kt
+++ src/jvmMain/kotlin/io/github/fourlastor/wilds_launcher/LatestReleaseDownloader.kt
@@ -4,15 +4,18 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
 import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.zip.ZipFile
 
 const val OWNER = "SheerSt"
 const val REPOSITORY = "pokewilds"
 
 const val READ_SIZE_IN_BYTES = 1024
+const val BUFFER_SIZE = 4096
 
 fun getLatestVersion() : String? {
     val url = URL("https://api.github.com/repos/$OWNER/$REPOSITORY/releases/latest")
@@ -45,7 +48,9 @@ fun downloadLatestRelease() {
         return
     }
 
-    val filename = "pokemon-wilds-windows64.zip"
+    val filenameWithoutExtension = "pokemon-wilds-windows64"
+    val filename = "$filenameWithoutExtension.zip"
+
     val url = URL("https://github.com/$OWNER/$REPOSITORY/releases/download/$latestVersion/$filename")
     val connection = url.openConnection()
     val totalBytesToDownload = connection.contentLength
@@ -81,4 +86,40 @@ fun downloadLatestRelease() {
     }
 
     println("Finished download of latest release.")
+
+    println("Unzipping file...")
+
+    val destination = filenameWithoutExtension
+
+    File(destination).run {
+        if (!exists()) {
+            mkdirs()
+        }
+    }
+
+    ZipFile(file).use { zipFile ->
+        zipFile.entries().asSequence().forEach { entry ->
+            zipFile.getInputStream(entry).use { inputStream ->
+                val path = destination + File.separator + entry.name
+
+                if (entry.isDirectory) {
+                    File(path).mkdir()
+                }
+                else {
+                    BufferedOutputStream(FileOutputStream(path)).use { outputStream ->
+                        val bytes = ByteArray(BUFFER_SIZE)
+                        var readBytes: Int
+
+                        while (inputStream.read(bytes).also { readBytes = it } != -1) {
+                            outputStream.write(bytes, 0, readBytes)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    println("Unzipped file.")
+
+    file.delete()
 }
