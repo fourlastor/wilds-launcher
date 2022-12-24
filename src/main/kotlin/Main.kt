058c6fe014d2c25f7bd4cb3189bc92a8f2e5e122
Daniele Conti <fourlastor@users.noreply.github.com>
2022-12-24 11:46:35 +0100

    Remove unneeded call

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 import io.github.fourlastor.wilds_launcher.app.DaggerAppComponent
-import javax.swing.UIManager
 
 fun main() {
-    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
     DaggerAppComponent.create().app().render()
 }
