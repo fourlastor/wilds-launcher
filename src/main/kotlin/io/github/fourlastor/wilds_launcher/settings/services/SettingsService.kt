cbe3ef845c31c1b628e046bb6b55aa365d339b62
Daniele Conti <fourlastor@users.noreply.github.com>
2022-12-24 11:46:50 +0100

    Use stateflow inside settings service

--- src/main/kotlin/io/github/fourlastor/wilds_launcher/settings/services/SettingsService.kt
+++ src/main/kotlin/io/github/fourlastor/wilds_launcher/settings/services/SettingsService.kt
@@ -2,6 +2,8 @@ package io.github.fourlastor.wilds_launcher.settings.services
 
 import io.github.fourlastor.wilds_launcher.app.Dirs
 import io.github.fourlastor.wilds_launcher.settings.Settings
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -13,7 +15,7 @@ import javax.inject.Singleton
 class SettingsService @Inject constructor(
     private val dirs: Dirs,
 ) {
-    private var settings: Settings = Settings()
+    private val settings = MutableStateFlow(Settings())
 
     private fun settingsFile(): File = File(dirs.config, "settings.json")
 
@@ -22,7 +24,7 @@ class SettingsService @Inject constructor(
             return
         }
 
-        settings = Json.decodeFromString(settingsFile().readText())
+        settings.update { Json.decodeFromString(settingsFile().readText()) }
     }
 
     private fun save() {
@@ -30,47 +32,47 @@ class SettingsService @Inject constructor(
             settingsFile().parentFile.mkdirs()
         }
 
-        settingsFile().writeText(Json.encodeToString(settings))
+        settingsFile().writeText(Json.encodeToString(settings.value))
     }
 
     fun clear() {
-        settings = Settings()
+        settings.update { Settings() }
         settingsFile().delete()
     }
 
     fun getJar(): String {
-        return settings.jar
+        return settings.value.jar
     }
 
     fun setJar(jar: String) {
-        settings = settings.copy(jar = jar)
+        settings.update { it.copy(jar = jar) }
         save()
     }
 
     fun getDevMode(): Boolean {
-        return settings.devMode
+        return settings.value.devMode
     }
 
     fun setDevMode(devMode: Boolean) {
-        settings = settings.copy(devMode = devMode)
+        settings.update { it.copy(devMode = devMode) }
         save()
     }
 
     fun getLogsEnabled(): Boolean {
-        return settings.logsEnabled
+        return settings.value.logsEnabled
     }
 
     fun setLogsEnabled(logsEnabled: Boolean) {
-        settings = settings.copy(logsEnabled = logsEnabled)
+        settings.update { it.copy(logsEnabled = logsEnabled) }
         save()
     }
 
     fun getAngleGles20(): Boolean {
-        return settings.angleGles20
+        return settings.value.angleGles20
     }
 
     fun setAngleGles20(angleGles20: Boolean) {
-        settings = settings.copy(angleGles20 = angleGles20)
+        settings.update { it.copy(angleGles20 = angleGles20) }
         save()
     }
 }
